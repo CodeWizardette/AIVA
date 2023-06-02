c9dd4530325ef64e19c4beafb87a108d13977370
CodeWizardette <131625021+CodeWizardette@users.noreply.github.com>
2023-06-03 00:15:54 +0300

    Add files via upload

---
+++ ai/main.py
@@ -0,0 +1,229 @@
+# Need ai moduls
+import os
+import sys
+import time
+import random
+import math
+import math
+import datetime
+import calendar
+import re
+import json
+import csv
+import pickle
+import hashlib
+import logging
+import threading
+import multiprocessing
+import boto3
+import requests
+import requests
+import urllib.request
+import urllib.parse
+import urllib.error
+import numpy as np
+from pandas import Series, DataFrame
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import r2_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import precision_recall_curve
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import style
+from matplotlib.pyplot import figure
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import matplotlib.dates as mdates
+import matplotlib.gridspec as gridspec
+from matplotlib.pyplot import show
+from matplotlib.pyplot import figure
+from matplotlib.pyplot import subplots
+import seaborn as sns
+from seaborn import set_style
+from seaborn import color_palette
+from seaborn import set_context
+from seaborn import set_context
+from seaborn import barplot
+from seaborn import boxplot
+from seaborn import violinplot
+from seaborn import boxplot
+from seaborn import swarmplot
+from seaborn import violinplot
+from seaborn import barplot
+from seaborn import countplot
+from seaborn import pointplot
+from seaborn import swarmplot
+from seaborn import kdeplot
+import tensorflow as tf
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.layers import Embedding
+from keras.layers import Activation
+from keras.layers import Embedding
+from keras.layers import Flatten
+from keras.layers import TimeDistributed
+import keras
+import sklearn
+from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import Normalizer
+from sklearn.preprocessing import Binarizer
+from sklearn.preprocessing import MaxAbsScaler
+from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import Normalizer
+from sklearn.preprocessing import Binarizer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Conv1D
+from tensorflow.keras.layers import MaxPooling1D
+from tensorflow.keras.layers import GlobalAveragePooling1D
+from tensorflow.keras.layers import GlobalMaxPooling1D
+from tensorflow.keras.layers import Bidirectional
+from tensorflow.keras.layers import GlobalAveragePooling1D
+from tensorflow.keras.layers import TimeDistributed
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Embedding
+import keras
+
+import sklearn
+import gensim
+import nltk
+import nltk
+import spacy
+import gensim
+import gensim.corpora as corpora
+from gensim.utils import simple_preprocess
+from gensim.models import CoherenceModel
+from gensim.models import Word2Vec
+from gensim.models import Phrases
+from gensim.models.phrases import Phraser
+from gensim.models.word2vec import LineSentence
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from gensim.test.utils import get_tmpfile
+from gensim.test.utils import get_tmpfile
+from gensim.corpora.dictionary import Dictionary
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from gensim.similarities import Similarity
+from gensim.parsing.preprocessing import STOPWORDS
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from gensim.models import KeyedVectors
+from gensim.scripts.glove2word2vec import glove2word2vec
+from gensim.test.utils import common_texts
+from gensim.test.utils import get_tmpfile
+from nltk.stem import WordNetLemmatizer
+from nltk.stem import PorterStemmer
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem import LancasterStemmer
+from nltk.stem import SnowballStemmer
+from nltk.stem import RegexpStemmer
+from nltk.stem import SnowballStemmer
+import beatifulsoup4
+from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
+import re
+import string
+import unicodedata
+
+# Gerekli modüllerin import edilmesi
+import modul1
+import modul2
+import modul3
+import modul4
+import modul5
+import modul6
+import modul7
+import modul7
+import modul8
+import modul9
+import modul10
+
+import byp
+
+# Use byp
+byp.byp()
+# Use byp
+
+#use bs4 for search all web for training ai models,upgraing ai models,learing,traing,
+
+
+# Search all web for training ai models 
+
+# Main Ai model starting here
+
+def main():
+    get_ai_model(modul1.model_name)
+    get_ai_model(modul2.model_name)
+    get_ai_model(modul3.model_name)
+    get_ai_model(modul4.model_name)
+    get_ai_model(modul5.model_name)
+    get_ai_model(modul6.model_name)
+    get_ai_model(modul7.model_name)
+    get_ai_model(modul8.model_name)
+    get_ai_model(modul8.model_name)
+    get_ai_model(modul9.model_name)
+    get_ai_model(modul10.model_name)
+
+
+# Search all web for training ai models 
+
+# Ana programın başlangıcı
+def ():
+    # Kodunuzun geri kalanını buraya yazabilirsiniz
+    print("Merhaba, yapay zeka projesine hoş geldiniz!")
+
+    # Diğer modülleri kullanarak yapay zeka kodunuzu burada geliştirebilirsiniz
+    modul1.fonksiyon1()
+    modul2.fonksiyon2()
+    modul3.fonksiyon3()
+    modul4.fonksiyon4()
+    modul5.fonksiyon5()
+    modul5.fonksiyon5()
+    modul6.fonksiyon6()
+    modul7.fonksiyon7()
+    modul8.fonksiyon8()
+    modul9.fonksiyon9()
+    modul10.fonksiyon10()
+
+
+# generate ai code here
+def get_ai_model(model_name):
+
+
+    # Programın sonu
+    print("Yapay zeka projesi tamamlandı!")
+
+# main() fonksiyonunun çağrılması
+if __name__ == '__main__':
+    main()
